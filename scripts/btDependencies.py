04687f3218cdf010abb20feabdef58099f43e17d
Matt Young <mfsy@yahoo.com>
2026-09-07 08:24:51 +0200

    Build fixes 6

--- scripts/btDependencies.py
+++ scripts/btDependencies.py
@@ -1238,5 +1238,10 @@ def installDependencies():
    btExecute.abortOnRunFail(subprocess.run(['cmake', '--install', './build', '--prefix', './build/dist', '--config', 'Release', '--verbose', '--component', 'sourcemeta_blaze']))
    btExecute.abortOnRunFail(subprocess.run(['cmake', '--install', './build', '--prefix', './build/dist', '--config', 'Release', '--verbose', '--component', 'sourcemeta_blaze_dev']))
 
+   blazeLibDirOutput = btExecute.abortOnRunFail(
+      subprocess.run(['ls', '-l', './dist/lib'], encoding = "utf-8", capture_output = True)
+   )
+   btLogger.log.debug('Blaze libs: ' + blazeLibDirOutput.stdout)
+
    btLogger.log.info('*** Finished checking / installing dependencies ***')
    return
